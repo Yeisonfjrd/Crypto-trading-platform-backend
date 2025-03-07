a36529973023763cd29eee37725cbc16c731c326
Yeisonfjrd <andresfajardo1606@gmail.com>
2025-03-07 11:10:37 -0300

    Add console log for DATABASE_URL in database configuration

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,8 @@
 import { Sequelize } from 'sequelize';
 import 'dotenv/config';
 
+console.log('DATABASE_URL en Railway:', process.env.DATABASE_URL || 'No definido');
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
